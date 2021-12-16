6d7129e4f693d8678951afd543a9f1e83d6f0567
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2021-12-16 12:35:09 +0000

    repository

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,11 @@
-import { useNavigation } from '@react-navigation/core';
-import React, { useRef, useState } from 'react';
-import { TextInput } from 'react-native';
+import { useNavigation } from "@react-navigation/core";
+import React, { useRef, useState } from "react";
+import { TextInput } from "react-native";
 
-import { Background } from '../../components/Background';
-import { Card } from '../../components/Card';
+import { Background } from "../../components/Background";
+import { Card } from "../../components/Card";
 
-import { useRepositories } from '../../hooks/useRepositories';
+import { useRepositories } from "../../hooks/useRepositories";
 
 import {
   Container,
@@ -16,10 +16,10 @@ import {
   InputButton,
   Icon,
   RepositoriesList
-} from './styles';
+} from "./styles";
 
 export function Dashboard() {
-  const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState("");
   const inputRef = useRef<TextInput>(null);
 
   const { navigate } = useNavigation();
@@ -27,55 +27,37 @@ export function Dashboard() {
   const { addRepository, repositories } = useRepositories();
 
   function handleAddRepository() {
-    /**
-     * TODO: 
-     * - call addRepository function sending inputText value;
-     * - clean inputText value.
-     */
+    addRepository(inputText);
+    setInputText("");
     inputRef.current?.blur();
   }
 
   function handleRepositoryPageNavigation(id: number) {
-    /**
-     * TODO - navigate to the Repository screen sending repository id.
-     * Remember to use the correct prop name (repositoryId) to the repositoy id:
-     * 
-     * navigate(SCREEN NAME, {
-     *  repositoryId: id of the repository
-     * })
-     */
+    navigate("Repository", { repositoryId: id });
   }
 
   return (
     <Background>
       <Container>
         <AddGithubRepo>
-          <Title>Explore repositórios{'\n'}no GitHub.</Title>
+          <Title>Explore repositórios{"\n"}no GitHub.</Title>
 
           <Input>
             <InputField
               ref={inputRef}
               placeholder="Digite aqui 'usuário/repositório'"
               value={inputText}
-              /**
-               * TODO - update inputText value when input text value 
-               * changes:
-               * onChangeText={YOUR CODE HERE}
-               */
+              onChangeText={setInputText}
               onSubmitEditing={handleAddRepository}
               returnKeyType="send"
-              autoCapitalize='none'
+              autoCapitalize="none"
               autoCorrect={false}
             />
 
             <InputButton
               testID="input-button"
               onPress={handleAddRepository}
-            /**
-             * TODO - ensure to disable button when inputText is 
-             * empty (use disabled prop to this):
-             * disabled={CONDITION HERE}
-             */
+              disabled={!inputText}
             >
               <Icon name="search" size={20} />
             </InputButton>
@@ -101,5 +83,5 @@ export function Dashboard() {
         />
       </Container>
     </Background>
-  )
+  );
 }
